b3a5414c2b87531bf8fed441b9daecf5a7048eb0
Plinio Productions <plinio.jrm@gmail.com>
2022-11-30 16:01:28 -0300

    fixing my mistake by not understanding what the tutorial was saying to me

--- getting-started/hello.go
+++ getting-started/hello.go
@@ -1,9 +1,9 @@
 package main
 
 import "fmt"
-//import "quote/v4" // hello.go:4:8: no required module provides package rsc.io/quote/v4; to add it: go get rsc.io/quote/v4
+import "rsc.io/quote/v4"
 
 func main()  {
 	fmt.Println("Hello, world")
-	//fmt.Println(quote.Go())
+	fmt.Println(quote.Go())
 }
