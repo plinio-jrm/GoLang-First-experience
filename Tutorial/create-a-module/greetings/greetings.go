95b0258c37ffd405e50093f2ab217838dc64bc28
Plinio Productions <plinio.jrm@gmail.com>
2022-11-30 19:14:16 -0300

    Completed 'return a random greeting' tutorial

--- Tutorial/create-a-module/greetings/greetings.go
+++ Tutorial/create-a-module/greetings/greetings.go
@@ -3,13 +3,29 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if (name == "") {
-		return "", errors.New("Empty name!")
+		return "", errors.New("Empty name!") 
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init()  {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hello, %v!!!",
+		"Heeeyyyy, you again %v? nice!",
+		"Hail, %v! oh my lord!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
