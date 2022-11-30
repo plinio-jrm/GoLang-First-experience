f78bb4ca73b7d4ca4052b4fd9f7ff3e206a325f6
Plinio Productions <plinio.jrm@gmail.com>
2022-11-30 19:31:39 -0300

    completed 'Return greetings for multiple people' tutorial

--- Tutorial/create-a-module/hello/hello.go
+++ Tutorial/create-a-module/hello/hello.go
@@ -11,10 +11,17 @@ func main()  {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
+	
 	message, err := greetings.Hello("Death Vader") 
+	if (err != nil) { 
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+	
+	names := []string{"Dah", "heh", "huh"}
+	messages, err := greetings.Hellos(names)
 	if (err != nil) {
 		log.Fatal(err)
 	}
-
-	fmt.Println(message)
+	fmt.Println(messages)
 }
